9e970948653c5553519ffbdaea8265199ae8f8d3
baa <34665120+baa-mb@users.noreply.github.com>
2023-08-26 18:10:49 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,56 @@
+function licht_servo () {
+    pins.servoWritePin(AnalogPin.P8, Math.map(input.lightLevel(), 0, 255, 0, 180))
+}
 input.onButtonPressed(Button.A, function () {
     lauf = true
 })
+function init () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    strip = neopixel.create(DigitalPin.P14, 4, NeoPixelMode.RGB)
+    strip.setBrightness(100)
+    strip.clear()
+    strip.show()
+    lauf = false
+    I2C_LCD1602.LcdInit(39)
+    I2C_LCD1602.ShowString("Klima-LF %:", 0, 0)
+    I2C_LCD1602.ShowString("Temperatur:", 0, 1)
+    pins.setAudioPin(AnalogPin.P1)
+    pins.setAudioPinEnabled(true)
+    gast = 0
+    I2C_LCD1602.BacklightOff()
+}
 input.onButtonPressed(Button.B, function () {
     lauf = false
 })
-let motor = false
-let lauf = false
-pins.digitalWritePin(DigitalPin.P1, 0)
-let strip = neopixel.create(DigitalPin.P14, 4, NeoPixelMode.RGB)
-strip.setBrightness(255)
-lauf = false
-I2C_LCD1602.LcdInit(39)
-I2C_LCD1602.ShowString("Klima", 0, 0)
-basic.forever(function () {
-    pins.servoWritePin(AnalogPin.P8, Math.map(input.lightLevel(), 0, 255, 0, 180))
-    if (input.lightLevel() < 50) {
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
-        pins.analogWritePin(AnalogPin.P16, 635)
+function besucher () {
+    basic.showNumber(pins.digitalReadPin(DigitalPin.P15))
+    if (pins.digitalReadPin(DigitalPin.P15) == 1) {
+        gast = 1
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     } else {
-        if (input.lightLevel() < 150) {
-            strip.showColor(neopixel.colors(NeoPixelColors.Orange))
-        } else {
-            strip.showColor(neopixel.colors(NeoPixelColors.Green))
-        }
-        pins.analogWritePin(AnalogPin.P16, 0)
+        gast = 0
     }
+    pins.digitalWritePin(DigitalPin.P16, gast)
+}
+function temperatur () {
+    dht11_dht22.queryData(
+    DHTtype.DHT11,
+    DigitalPin.P2,
+    true,
+    false,
+    true
+    )
+    if (dht11_dht22.readDataSuccessful()) {
+        I2C_LCD1602.BacklightOn()
+        I2C_LCD1602.ShowNumber(dht11_dht22.readData(dataType.humidity), 12, 0)
+        I2C_LCD1602.ShowNumber(dht11_dht22.readData(dataType.temperature), 12, 1)
+        basic.pause(5000)
+        I2C_LCD1602.BacklightOff()
+    }
+}
+function motoren () {
     if (lauf) {
         motor = true
         pins.analogWritePin(AnalogPin.P12, 200)
@@ -46,21 +72,15 @@ basic.forever(function () {
             pins.digitalWritePin(DigitalPin.P13, 0)
             motor = false
         }
-        dht11_dht22.queryData(
-        DHTtype.DHT11,
-        DigitalPin.P2,
-        true,
-        false,
-        true
-        )
-        if (dht11_dht22.readDataSuccessful()) {
-            I2C_LCD1602.ShowNumber(dht11_dht22.readData(dataType.humidity), 12, 0)
-            I2C_LCD1602.ShowNumber(dht11_dht22.readData(dataType.temperature), 12, 1)
-            I2C_LCD1602.ShowNumber(1, 2, 1)
-        } else {
-            I2C_LCD1602.ShowNumber(0, 2, 1)
-        }
     }
-    I2C_LCD1602.ShowString(convertToText(input.lightLevel() + 1000).substr(1, 3), 8, 1)
     basic.pause(2000)
+}
+let motor = false
+let gast = 0
+let strip: neopixel.Strip = null
+let lauf = false
+basic.showIcon(IconNames.Yes)
+init()
+basic.forever(function () {
+    besucher()
 })
