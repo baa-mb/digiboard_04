14299e7d355e6998296351d06c1333b9293a0e0c
baa <34665120+baa-mb@users.noreply.github.com>
2023-08-27 03:12:08 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-function licht_servo() {
+function licht_servo () {
     pins.servoWritePin(AnalogPin.P8, licht)
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
     if (licht == 0) {
@@ -11,7 +11,7 @@ function licht_servo() {
 input.onButtonPressed(Button.A, function () {
     motor_start = true
 })
-function init() {
+function init () {
     // pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P16, 0)
@@ -30,7 +30,7 @@ function init() {
     basic.clearScreen()
     init_variable()
 }
-function get_winkel(arr: number[]) {
+function get_winkel (arr: number[]) {
     flag = arr[1]
     num = arr[0]
     num = (num + add_winkel[flag]) % (180 + add_winkel[flag])
@@ -39,7 +39,7 @@ function get_winkel(arr: number[]) {
 input.onButtonPressed(Button.B, function () {
     motor_start = false
 })
-function besucher() {
+function besucher () {
     // music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
     if (pins.digitalReadPin(DigitalPin.P15) == 1) {
         gast = 1
@@ -48,13 +48,13 @@ function besucher() {
     }
     pins.digitalWritePin(DigitalPin.P16, gast)
 }
-function temperatur() {
+function temperatur () {
     dht11_dht22.queryData(
-        DHTtype.DHT11,
-        DigitalPin.P2,
-        true,
-        false,
-        true
+    DHTtype.DHT11,
+    DigitalPin.P2,
+    true,
+    false,
+    true
     )
     if (dht11_dht22.readDataSuccessful()) {
         // if (control.millis() - temp_zeit > temp_interval || dht11_dht22.readData(dataType.temperature) != old_temp) {
@@ -69,7 +69,7 @@ function temperatur() {
         }
     }
 }
-function init_variable() {
+function init_variable () {
     add_winkel = [45, 10]
     licht = 0
     gast = 0
@@ -77,26 +77,25 @@ function init_variable() {
     temp_interval = 15000
     solar_winkel = 90
 }
-function motoren() {
+function motoren () {
     if (motor_start) {
         motor = true
         pins.analogWritePin(AnalogPin.P12, 200)
         pins.analogWritePin(AnalogPin.P13, 0)
         // if (motor_start) {
-        //     basic.pause(5000)
-        //     pins.digitalWritePin(DigitalPin.P12, 0)
-        //     pins.digitalWritePin(DigitalPin.P13, 0)
-        //     if (motor_start) {
-        //         basic.pause(1000)
-        //         pins.analogWritePin(AnalogPin.P12, 0)
-        //         pins.analogWritePin(AnalogPin.P13, 300)
-        //         basic.pause(5000)
-        //     }
-        //     basic.pause(2000)
+        // basic.pause(5000)
+        // pins.digitalWritePin(DigitalPin.P12, 0)
+        // pins.digitalWritePin(DigitalPin.P13, 0)
+        // if (motor_start) {
+        // basic.pause(1000)
+        // pins.analogWritePin(AnalogPin.P12, 0)
+        // pins.analogWritePin(AnalogPin.P13, 300)
+        // basic.pause(5000)
+        // }
+        // basic.pause(2000)
         // }
         // } else {
-
-        basic.pause(2000)
+        basic.pause(4000)
         if (motor) {
             pins.digitalWritePin(DigitalPin.P12, 0)
             pins.digitalWritePin(DigitalPin.P13, 0)
@@ -105,7 +104,7 @@ function motoren() {
         }
     }
 }
-function motorstart() {
+function motorstart () {
     if (pins.analogReadPin(AnalogPin.P0) > 300) {
         motor_start = true
     }
@@ -113,6 +112,7 @@ function motorstart() {
 let motor = false
 let temp_interval = 0
 let temp_zeit = 0
+let old_humidity = 0
 let gast = 0
 let add_winkel: number[] = []
 let num = 0
@@ -122,12 +122,9 @@ let solar_winkel = 0
 let strip: neopixel.Strip = null
 let licht = 0
 let num2 = 0
-let old_humidity=0
 basic.showIcon(IconNames.Yes)
 init()
 serial.redirectToUSB()
-
-
 basic.forever(function () {
     serial.writeValue("x", pins.analogReadPin(AnalogPin.P0))
     besucher()
